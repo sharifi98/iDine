990720cbef93a6d9c9e97a6f0648c33639602049
Hossein Sharifi <47387238+sharifi98@users.noreply.github.com>
2023-05-04 10:49:35 +0200

    Initial Commit

---
+++ BuildingACompleteProject/BuildingACompleteProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuildingACompleteProjectApp.swift
+//  BuildingACompleteProject
+//
+//  Created by Hossein Sharifi on 04/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct BuildingACompleteProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
